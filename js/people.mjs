ad80868107ac2afb188de412b83d900857e4d72d
KobeLemon <kobe42lemon@outlook.com>
2023-10-01 15:18:21 -0600

    Created planets/planets.mjs functionality

--- js/people.mjs
+++ js/people.mjs
@@ -4,45 +4,25 @@ let template = '';
 let yearTitle = '';
 const naArray = ['n/a', 'N/a,', 'n/A', 'N/A', 'na', 'NA']
 
-const personPreviewTemplateFunc = (person) => {
+const personTemplateFunc = (person) => {
     if (naArray.includes(person.gender)) {
-        yearTitle = "Creation Year:"
+        yearTitle = "Year of Creation:"
     } else {
-        yearTitle = "Birth Year:"
+        yearTitle = "Year of Birth:"
     }
 
     return template = 
     `<div class="tile">
         <img class="personImg" src="../placeholder75x75.png" alt="Picture of ${person.name}">
         <p class="personName">${person.name}</p>
+        <p class="personHeight">Height: ${person.height} cm</p>
+        <p class="personWeight">Weight: ${person.mass} kg</p>
+        <p class="personHairColor">Hair Color: ${capitalizeSentence(person.hair_color)}</p>
+        <p class="personSkinColor">Skin Color: ${capitalizeSentence(person.skin_color)}</p>
+        <p class="personEyeColor">Eye Color: ${capitalizeSentence(person.eye_color)}</p>
         <p class="personBirthYear">${yearTitle} ${person.birth_year}</p>
         <p class="personGender">Gender: ${capitalizeSentence(person.gender)}</p>
-        <button>Learn More!</button>
-        <p id="swapiLink">${person.url}</p>
     </div>`;
 };
 
-const personFullTemplateFunc = (person) => {
-    if (naArray.includes(person.gender)) {
-        yearTitle = "Creation Year:"
-    } else {
-        yearTitle = "Birth Year:"
-    }
-
-    return template = 
-    `<div class="tile">
-        <img class="personImg" src="../placeholder75x75.png" alt="Picture of ${person.name}">
-        <p class="personName">${person.name}</p>
-        <p class="personHeight">${person.height}</p>
-        <p class="personWeight">${person.mass}</p>
-        <p class="personHairColor">${capitalizeSentence(person.hair_color)}</p>
-        <p class="personSkinColor">${capitalizeSentence(person.skin_color)}</p>
-        <p class="personEyeColor">${capitalizeSentence(person.eye_color)}</p>
-        <p class="personBirthYear">${yearTitle} ${person.birth_year}</p>
-        <p class="personGender">Gender: ${capitalizeSentence(person.gender)}</p>
-        <button>Learn More!</button>
-        <p id="swapiLink">${person.url}</p>
-    </div>`
-};
-
-renderApiInfo('https://swapi.dev/api/people', personPreviewTemplateFunc)
+renderApiInfo('https://swapi.dev/api/people', personTemplateFunc)
---
+++ js/planets.mjs
@@ -0,0 +1,20 @@
+import { capitalizeSentence, renderApiInfo } from "./utils.mjs";
+
+let template = '';
+
+const planetTemplateFunc = (planet) => {
+    return template = 
+    `<div class="tile">
+        <img class="planetImg" src="../placeholder75x75.png" alt="Picture of ${planet.name}">
+        <p class="planetName">${planet.name}</p>
+        <p class="planetday">Day: ${planet.rotation_period} hours</p>
+        <p class="planetYear">Year: ${planet.orbital_period} days</p>
+        <p class="planetDiameter">Diameter: ${planet.diameter} km</p>
+        <p class="planetClimate">Climate: ${capitalizeSentence(planet.climate)}</p>
+        <p class="planetGravity">Gravity: ${capitalizeSentence(planet.gravity)}</p>
+        <p class="planetTerrain">Terrain: ${capitalizeSentence(planet.terrain)}</p>
+        <p class="planetPopulation">Population: ${planet.population}</p>
+    </div>`
+}
+
+renderApiInfo('https://swapi.dev/api/planets', planetTemplateFunc)
